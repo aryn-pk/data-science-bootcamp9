274266ce5f4e2a5b17589060e7785ea831f15d2d
aryn-pk <162943060+aryn-pk@users.noreply.github.com>
2024-03-11 06:53:00 +0000

    add line 4-16

--- hello.R
+++ hello.R
@@ -1,2 +1,16 @@
 ## this is R programing
 print('hello world')
+
+print('this is a second line')
+
+library(dplyr)
+
+df <- read_csv('filename.csv')
+
+df %>% 
+  filter(col>100)
+
+df %>%
+  select(1:10) %>%
+  filter(country == 'Thailand') %>%
+  arrange(-income)
